4093d25eb39b3a31a166e8c1557ef730220a1b46
Javier Poyatos <jpoyatosamador@correo.ugr.es>
2018-05-15 18:41:09 +0200

    Surface Games

---
+++ src/surfacegames/AnimatedGamePanel.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package surfacegames;
+
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author Javier
+ */
+public abstract class AnimatedGamePanel extends GamePanel implements ActionListener {
+    
+    
+    
+}
